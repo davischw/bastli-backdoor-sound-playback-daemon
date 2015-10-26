6b0ea0d2af3358b09ecb987ca48e773252f0beb7
Dominik Boehi <dominik.boehi@gmail.com>
2015-10-26 14:45:46 +0100

    changed msg to bytes in accordance with rmq docu

--- rmq_interface.c
+++ rmq_interface.c
@@ -2,25 +2,25 @@
 
 #include "rmq_interface.h"
 
-char *amqp_bytes_to_str(const amqp_bytes_t *msg)
+char *amqp_bytes_to_str(const amqp_bytes_t *bytes)
 {
         char *str = NULL;
         
-        if(NULL == msg)
+        if(NULL == bytes)
         {
                 return NULL;
         }
 
-        str = (char *) malloc((msg->len + 1) * sizeof(char));
+        str = (char *) malloc((bytes->len + 1) * sizeof(char));
 
         if(NULL == str)
         {
-                fprintf(stderr, "Could not allocate %ld bytes. Exiting.\n", (msg->len + 1));
+                fprintf(stderr, "Could not allocate %zu bytes. Exiting.\n", (bytes->len + 1));
                 exit(7);
         }
 
-        memcpy(str, msg->bytes, msg->len);
-        str[msg->len] = '\0';
+        memcpy(str, bytes->bytes, bytes->len);
+        str[bytes->len] = '\0';
 
         return str;
 }
--- rmq_interface.h
+++ rmq_interface.h
@@ -15,11 +15,11 @@
  * converts amqp_bytes_t into a c string.
  * allocates space for a new string.
  * @NOTE caller has to free alloc'd memory after use.
- * @PARAM[in] msg pointer to a amqp bytes object.
+ * @PARAM[in] bytes pointer to a amqp bytes object.
  * @RETURN a pointer to the allocated string containing
  * the message string.
  * @RETURN NULL if msg is NULL.
  */
-char *amqp_msg_to_str(const amqp_bytes_t *msg);
+char *amqp_bytes_to_str(const amqp_bytes_t *bytes);
 
 #endif /* _RMQ_INTERFACE_H_ */

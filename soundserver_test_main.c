83be09b8a405f7f6a7a98d98dc69cb575a31779e
troll@bastli.ethz.ch <troll@bastli.ethz.ch>
2015-10-30 18:16:45 +0100

    fixed failsound

--- soundserver_test_main.c
+++ soundserver_test_main.c
@@ -21,7 +21,7 @@
 // needs slash at end
 const char *sounds_dir = "std_sounds/";
 
-const char *failsound = "jungle2.wav";
+const char *failsound = "std_sounds/jungle2.wav";
 
 
 int main(int argc, char **argv)
